473601c0127d3527baa4b088ba669a8684eb23f3
o-strekalova <strekalova.olg@yandex.ru>
2020-06-17 02:22:16 +0300

    Добавляет валидацию пропсов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,21 @@
-import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
+import React from "react";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {placesCount} = props;
+  const {placesCount, offersTitles} = props;
 
   return (
     <Main
       placesCount={placesCount}
+      offersTitles={offersTitles}
     />
   );
+};
+
+App.propTypes = {
+  placesCount: PropTypes.number.isRequired,
+  offersTitles: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const PLACES_COUNT = 312;
+const OFFERS_TITLES = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
 
 ReactDOM.render(
     <App
       placesCount={PLACES_COUNT}
+      offersTitles={OFFERS_TITLES}
     />,
     document.querySelector(`#root`)
 );
